9a41d5c2aea747bde9cda79667d53f587d1ede5f
v-senthil <66490366+v-senthil@users.noreply.github.com>
2020-06-06 00:21:15 +0530

    Add files via upload

---
+++ automation/automation.ino
@@ -0,0 +1,32 @@
+#include <SoftwareSerial.h>
+SoftwareSerial BTSerial(2,3); // RX | TX
+int BULB1 = 4;
+
+void setup() 
+{
+  pinMode(BULB1,OUTPUT); // Trigger for Bulb 1
+  Serial.begin(9600);
+  BTSerial.begin(9600);
+}
+
+void loop() {
+  if(BTSerial.available())
+  {
+    Serial.println("BTSerial is available");
+    char data=BTSerial.read();
+    switch (data) {
+    case 'a':
+      digitalWrite(BULB1,1);
+      Serial.println("Changed the BULB1 value to HIGH");
+      break;
+    case 'b':
+      digitalWrite(BULB1,0);
+      Serial.println("Changed the BULB1 value to LOW");
+      break;
+    default: 
+      // if nothing else matches, do the default
+      // default is optional
+    break;
+    }
+  }
+}
